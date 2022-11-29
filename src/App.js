aa44ee4cd990f75e67b48142a0ff5df8e5c58c69
Saran M <saran.m@tataelxsi.co.in>
2022-11-29 16:48:54 +0530

    added flex , pagination, undo, redo and more

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GridDisplay from "./components/grid/GridDisplay";
+import GridDataUpdate from "./components/grid/GridDataUpdate";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="grid-data" element={<GridDisplay />} />
+          <Route path="update-data" element={<GridDataUpdate />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/grid/GridDisplay.js
+++ src/components/grid/GridDisplay.js
@@ -1,21 +1,20 @@
-import { useState, useEffect, useMemo, useRef } from 'react';
+import { useState, useEffect, useMemo, useRef } from "react";
 // import { useSelector, connect } from 'react-redux';
-import { AgGridReact } from 'ag-grid-react';
+import { AgGridReact } from "ag-grid-react";
 // import { fetchData, fetchDataWithAxios } from '../../redux/usersData/userReducer';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 // import store from '../../redux/store';
-import axios from 'axios';
+import axios from "axios";
 
-
-const simpleComp = p => {
-  console.log(p.btnName)
-  return(
+const simpleComp = (p) => {
+  console.log(p.btnName);
+  return (
     <>
-    <button onClick={() => alert(`${p.value}`)}>{p.btnName}</button>
+      <button onClick={() => alert(`${p.value}`)}>{p.btnName}</button>
     </>
-  )
-}
+  );
+};
 
 // const simpleComp1 = p => {
 //   console.log(p.btnName)
@@ -27,7 +26,6 @@ const simpleComp = p => {
 //   )
 // }
 
-
 const GridDisplay = (props) => {
   // const users = useSelector((state) => state.user);
   const [rowData, setRowData] = useState([]);
@@ -38,23 +36,64 @@ const GridDisplay = (props) => {
   const floatingFiltersHeight = 70;
 
   const [columnDefs, setColumnDefs] = useState([
-    { headerName: 'Row ID', field: 'id', valueGetter: 'node.id' },
-    {field: 'name', width: 150, resizable: true, pinned: 'left', lockPinned: true, filter: 'agTextColumnFilter',  rowDrag: true,
+    { headerName: "Row ID", field: "id", valueGetter: "node.id" },
+    {
+      field: "name",
+      width: 150,
+      resizable: true,
+      pinned: "left",
+      lockPinned: true,
+      filter: "agTextColumnFilter",
+      rowDrag: true,
       headerCheckboxSelection: true,
       checkboxSelection: true,
-      showDisabledCheckboxes: true,},
-    {field: 'username', colSpan: p => p.data.username === "Bret" ? 2 : 1},
-    {field: 'email', cellRenderer: simpleComp, cellRendererParams: {btnName: 'See'}},
-    {field: 'website', rowSpan: p => p.data.id === 7 ? 2 : 1},
-    {headerName: 'Address',
-      colId: 'address',
+      showDisabledCheckboxes: true,
+    },
+    { field: "username", colSpan: (p) => (p.data.username === "Bret" ? 2 : 1) },
+    {
+      field: "email",
+      cellRenderer: simpleComp,
+      cellRendererParams: { btnName: "See" },
+    },
+    { field: "website", rowSpan: (p) => (p.data.id === 7 ? 2 : 1) },
+    {
+      headerName: "Address",
+      pinned: "right",
+      colId: "address",
       children: [
-        {headerName: 'Street',field: 'address.street', colId:"street"},
-        {headerName: 'City',field: 'address.city', colId:"city"},
-        {headerName: 'Zipcode',field: 'address.zipcode', colId:"zipcode", filter: 'agNumberColumnFilter'}
-      ]
-    }
-    
+        {
+          headerName: "Street",
+          field: "address.street",
+          colId: "street",
+          cellEditor: "agLargeTextCellEditor",
+          cellEditorPopup: true,
+          cellEditorParams: {
+            maxLength: 100,
+            rows: 1,
+            cols: 40,
+          },
+        },
+        {
+          headerName: "City",
+          field: "address.city",
+          colId: "city",
+          columnGroupShow: "open",
+          cellEditor: "agSelectCellEditor",
+          cellEditorPopup: true,
+          cellEditorParams: {
+            cellHeight: 50,
+            values: ["New York", "Chennai", "Sydney"],
+          },
+        },
+        {
+          headerName: "Zipcode",
+          field: "address.zipcode",
+          colId: "zipcode",
+          filter: "agNumberColumnFilter",
+          columnGroupShow: "open",
+        },
+      ],
+    },
   ]);
 
   // const Sidebar = useMemo(() => {
@@ -78,40 +117,40 @@ const GridDisplay = (props) => {
   //   }
 
   //   fetchUser();
-  
-  //  }, []);
 
+  //  }, []);
 
   const getRowId = useMemo(() => {
     return (params) => params.data.id;
   }, []);
 
-
   useEffect(() => {
     fetchDataWithAxios();
-  }, [])
+  }, []);
 
-  const fetchDataWithAxios = async() => {
-        try {
-            const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-            setRowData(res.data);
-          } catch(err) {
-            console.log(err.message);
-          }
+  const fetchDataWithAxios = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setRowData(res.data);
+    } catch (err) {
+      console.log(err.message);
     }
-   
+  };
 
-  const defaultColDef = useMemo( ()=> ({
-    sortable: true,
-    editable: true,
-    // flex: 1,
-    filterParams: {
-      buttons: ['reset', 'apply'],
-      debounceMs: 0
-    },
-    floatingFilter: true,
-    enableRowGroup: true
-  }), []);
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      editable: true,
+      resizable: true,
+      flex: 1,
+      filterParams: {
+        buttons: ["reset", "apply"],
+      },
+      floatingFilter: true,
+      enableRowGroup: true,
+    }),
+    []
+  );
 
   const isRowSelectable = useMemo(() => {
     return (params) => {
@@ -125,16 +164,16 @@ const GridDisplay = (props) => {
     const filterModel = gridRef.current.api.getFilterModel();
     console.log(filterModel);
     savedFilterState.current = filterModel;
-  }
+  };
 
   const onApplyBtn = () => {
     const filterModel = savedFilterState.current;
     gridRef.current.api.setFilterModel(filterModel);
-  }
+  };
 
   const onDeselectBtn = () => {
     gridRef.current.api.deselectAll();
-  }
+  };
 
   // const onBtnGrpEmailWebsite = () => {
   //   gridRef.current.columnApi.applyColumnState({
@@ -148,93 +187,138 @@ const GridDisplay = (props) => {
 
   const removeEmailBtn = () => {
     gridRef.current.columnApi.applyColumnState({
-      state: [
-        {colId: 'email', hide: true}
-      ]
+      state: [{ colId: "email", hide: true }],
     });
-  }
+  };
 
   const showEmailBtn = () => {
     gridRef.current.columnApi.applyColumnState({
-      state: [
-        {colId: 'email', hide: false}
-      ]
+      state: [{ colId: "email", hide: false }],
     });
-  }
-
+  };
 
   const saveState = () => {
     window.colState = gridRef.current.columnApi.getColumnState();
-    console.log('column state saved');
-  }
+    console.log("column state saved");
+  };
 
   const restoreState = () => {
     gridRef.current.columnApi.applyColumnState({
       state: window.colState,
-      applyOrder: true
+      applyOrder: true,
     });
-  }
+  };
 
   const resetState = () => {
     gridRef.current.columnApi.resetColumnState();
-  }
+  };
 
   const idToLastBtn = () => {
     gridRef.current.columnApi.applyColumnState({
       state: [
-        {colId: 'name'},
-        {colId: 'username'},
-        {colId: 'email'},
-        {colId: 'website'},
-        {colId: 'street'},
-        {colId: 'city'},
-        {colId: 'zipcode'},
-        {colId: 'id'},
-
+        { colId: "name" },
+        { colId: "username" },
+        { colId: "email" },
+        { colId: "website" },
+        { colId: "street" },
+        { colId: "city" },
+        { colId: "zipcode" },
+        { colId: "id" },
       ],
-      applyOrder: true
+      applyOrder: true,
     });
-  }
+  };
 
+  // console.log(gridRef.current.api.getRowNode('id'));
+
+  const forEachNodeHandler = () => {
+    gridRef.current.api.forEachNode((node, index) => {
+      console.log(`${index} -> ${node.data.username}`);
+    });
+  };
+
+  const forEachNodeAfterFilterHandler = () => {
+    gridRef.current.api.forEachNodeAfterFilter((node, index) => {
+      console.log(`${index} -> ${node.data.username}`);
+    });
+  };
+
+  const forEachNodeAfterFilterAndSortHandler = () => {
+    gridRef.current.api.forEachNodeAfterFilterAndSort((node, index) => {
+      console.log(`${index} -> ${node.data.username}`);
+    });
+  };
+
+  const forEachLeafNodeHandler = () => {
+    gridRef.current.api.forEachLeafNode((node, index) => {
+      console.log(`${index} -> ${node.data.username}`);
+    });
+  };
 
   return (
-        <>
-          <button type='button' onClick={saveState}>Save State</button>
-          <button type='button' onClick={restoreState}>Restore State</button>
-          <button type='button' onClick={resetState}>Reset State</button>
-          <button type='button' onClick={onSaveBtn}>Save Filter</button>
-          <button type='button' onClick={onApplyBtn}>Restore Filter</button>
-          <button type='button' onClick={onDeselectBtn}>Deselect All</button>
-          <button type='button' onClick={removeEmailBtn}>Remove Email</button>
-          <button type='button' onClick={showEmailBtn}>Show Email</button>
-          <button type='button' onClick={idToLastBtn}>ID to Last</button>
-          {/* <button type='button' onClick={onBtnGrpEmailWebsite}>Group Email - Website</button> */}
-       
-          <div className="ag-theme-alpine" style={{width: 1200, height: 1000}}>
-            <AgGridReact ref={gridRef} 
-            columnDefs={columnDefs} 
-            rowData={rowData} 
-            defaultColDef={defaultColDef} 
-            rowSelection={'multiple'} 
-            animateRows={true}
-            headerHeight={headerHeight}
-            groupHeaderHeight={groupHeaderHeight}
-            floatingFiltersHeight={floatingFiltersHeight}
-            getRowId={getRowId}
-            suppressRowTransform={true}
-            rowDragManaged={true}
-            rowMultiSelectWithClick={true}
-            isRowSelectable={isRowSelectable}>
-       
-            </AgGridReact>
-            
-          </div>
-      
-    </>
+    <>
+      <button type="button" onClick={saveState}>
+        Save State
+      </button>
+      <button type="button" onClick={restoreState}>
+        Restore State
+      </button>
+      <button type="button" onClick={resetState}>
+        Reset State
+      </button>
+      <button type="button" onClick={onSaveBtn}>
+        Save Filter
+      </button>
+      <button type="button" onClick={onApplyBtn}>
+        Restore Filter
+      </button>
+      <button type="button" onClick={onDeselectBtn}>
+        Deselect All
+      </button>
+      <button type="button" onClick={removeEmailBtn}>
+        Remove Email
+      </button>
+      <button type="button" onClick={showEmailBtn}>
+        Show Email
+      </button>
+      <button type="button" onClick={idToLastBtn}>
+        ID to Last
+      </button>
+      <button type="button" onClick={forEachNodeHandler}>
+        For Each Node
+      </button>
+      <button type="button" onClick={forEachNodeAfterFilterHandler}>
+        For Each Node After Filter
+      </button>
+      <button type="button" onClick={forEachNodeAfterFilterAndSortHandler}>
+        For Each Node After Filter and Sort
+      </button>
+      <button type="button" onClick={forEachLeafNodeHandler}>
+        For Each Leaf Node
+      </button>
+      {/* <button type='button' onClick={onBtnGrpEmailWebsite}>Group Email - Website</button> */}
 
+      <div className="ag-theme-alpine" style={{ width: 1200, height: 1000 }}>
+        <AgGridReact
+          ref={gridRef}
+          columnDefs={columnDefs}
+          rowData={rowData}
+          defaultColDef={defaultColDef}
+          rowSelection={"multiple"}
+          animateRows={true}
+          headerHeight={headerHeight}
+          groupHeaderHeight={groupHeaderHeight}
+          floatingFiltersHeight={floatingFiltersHeight}
+          getRowId={getRowId}
+          suppressRowTransform={true}
+          rowDragManaged={true}
+          rowMultiSelectWithClick={true}
+          isRowSelectable={isRowSelectable}
+        ></AgGridReact>
+      </div>
+    </>
   );
 };
-
 
 // const mapStateToProps = (state) => {
 //     return {
